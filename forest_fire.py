bb36340e6e6e41d28e3681671d91939118dc6f9e
Methacrylon <methacrylon@gmail.com>
2019-05-07 22:49:13 +0200

    Basic main

---
+++ forest_fire.py
@@ -0,0 +1,16 @@
+"""
+Main module.
+"""
+import argparse
+
+def main():
+    """
+    Main function.
+    Parse arguments and run the simulator.
+    """
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
