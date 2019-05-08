911206a7bfbcda6795f7515bae50b44be4c0bcbe
Methacrylon <methacrylon@gmail.com>
2019-05-08 15:10:32 +0200

    Make BaserRenderer an abstract class

--- src/renderer/base.py
+++ src/renderer/base.py
@@ -1,6 +1,18 @@
-class BaseRenderer:
+"""
+Abstract renderer. Renderer should extend from the BaseRenderer.
+"""
+from abc import ABC, abstractmethod
+
+class BaseRenderer(ABC):
+    """
+    Abstract renderer. Specify the interface of a renderer.
+    """
     def __init__(self, grid):
         self.grid = grid
 
+    @abstractmethod
     def render(self):
-        pass
+        """
+        Should render the forest.
+        """
+        raise NotImplementedError   # pragma: no cover
