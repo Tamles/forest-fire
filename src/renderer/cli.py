8688c402b180784b8d0929ec243fbef9e142b706
Methacrylon <methacrylon@gmail.com>
2019-05-08 14:53:51 +0200

    Refacto of the CLI renderer: use list comprehension for render; add documentation.

--- src/renderer/cli.py
+++ src/renderer/cli.py
@@ -1,18 +1,32 @@
+"""
+Module to render a forest in the console.
+"""
+
 import src.renderer.base as base
 import src.engine.grid as gd
 
 class CLIRenderer(base.BaseRenderer):
+    """
+    CLI Renderer.
+    Extend from the base renderer and implement the render method.
+    """
+
     def __init__(self, grid):
         super().__init__(grid)
 
     def render(self):
-        display = ""
-        for y in range(self.grid.height):
-            for x in range(self.grid.width):
-                display += self._render_cell(self.grid[y][x])
-            display += "\n"
-        return display[:~0]
+        """
+        Return a string with the state of the forest.
+        Each row is separated by a newline.
+        Each cell in a row is next to the previous one.
+        """
+        display = "\n".join("".join([self._render_cell(cell) for cell in row]) for row in self.grid)
+        return display
 
-    def _render_cell(self, cell):
-        CELLS = {gd.EMPTY: '.', gd.TREE: 'o', gd.BURNING: 'x'}
-        return CELLS[cell]
+    @staticmethod
+    def _render_cell(cell):
+        """
+        Return a character based on the state of the cell.
+        """
+        cells = {gd.EMPTY: '.', gd.TREE: 'o', gd.BURNING: 'x'}
+        return cells[cell]
