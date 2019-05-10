56e2bc2cad4554d3c2c6374d12569c9385f093c9
Methacrylon <methacrylon@gmail.com>
2019-05-10 17:06:08 +0200

    forest_fire.py is the entry point to run the simulation. It can be run with parameters to customize the simulation.

--- forest_fire.py
+++ forest_fire.py
@@ -7,29 +7,42 @@ import time
 import src.engine.grid as gd
 import src.renderer.cli as cli_rd
 
+def probability(x):
+    x = float(x)
+    if x < 0 or x > 1:
+        raise argparse.ArgumentTypeError("{} is not a valid probability, should be between 0 and 1".format(x))
+    return x
+
+def positive_int(x):
+    x = int(x)
+    if x < 0:
+        raise argparse.ArgumentTypeError("{} is not a positive integer, should be 0 or higher".format(x))
+    return x
+
+def positive_float(x):
+    x = float(x)
+    if x < 0:
+        raise argparse.ArgumentTypeError("{} is not a positive float, should be 0.0 or higher".format(x))
+    return x
+
 def main():
     """
     Main function.
     Parse arguments and run the simulator.
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Run a simulation of forest fire")
+    parser.add_argument('--update_rate', type=positive_float, help="Time between two update of the forest, in seconds", default=0.2)
+    parser.add_argument('--number_steps', type=positive_int, help="Number of steps before the simulation ends", default=10)
+    parser.add_argument('--planting_rate', type=probability, help="Probability of a tree to grow on an empty cell", default=0.1)
+    parser.add_argument('--lightning_rate', type=probability, help="Probability of a light to strike each step", default=0.1)
     args = parser.parse_args()
-    grid = gd.Grid(13, 13, 0.1)
-    for x in range(grid.width):
-        for y in range(grid.height):
-            if random.random() > 0.4:
-                grid[x, y] = gd.TREE
-
-    renderer = cli_rd.CLIRenderer(grid)
+    update_rate = args.update_rate
+    number_steps = args.number_steps
+    planting_rate = args.planting_rate
+    lightning_rate = args.lightning_rate
+    grid = gd.Grid(13, 13, planting_rate=planting_rate, lightning_rate=lightning_rate)
+    renderer = cli_rd.CLIRenderer(grid, update_rate=update_rate, number_steps=number_steps)
     renderer.render()
-    print()
-    grid[6, 0] = 'burning'
-    renderer.render()
-    while gd.BURNING in grid:
-        time.sleep(0.2)
-        grid.update()
-        print()
-        renderer.render()
 
 if __name__ == "__main__":
     main()
