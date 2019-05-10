b01bc49da07578c9ca7e8bab8c416f9b4c6f9a0b
Methacrylon <methacrylon@gmail.com>
2019-05-10 11:07:17 +0200

    Main: run update until there is no more fire

--- forest_fire.py
+++ forest_fire.py
@@ -2,6 +2,8 @@
 Main module.
 """
 import argparse
+import random
+import time
 import src.engine.grid as gd
 import src.renderer.cli as cli_rd
 
@@ -15,34 +17,19 @@ def main():
     grid = gd.Grid(13, 13)
     for x in range(grid.width):
         for y in range(grid.height):
-            grid[x, y] = gd.TREE
+            if random.random() > 0.4:
+                grid[x, y] = gd.TREE
 
     renderer = cli_rd.CLIRenderer(grid)
     renderer.render()
     print()
-    grid[6, 6] = 'burning'
+    grid[6, 0] = 'burning'
     renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
-    grid.update()
-    print()
-    renderer.render()
+    while gd.BURNING in grid:
+        time.sleep(0.2)
+        grid.update()
+        print()
+        renderer.render()
 
 if __name__ == "__main__":
     main()
