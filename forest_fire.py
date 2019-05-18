6161f50427314f99f0b2564aa3c10a48d3dc8389
Methacrylon <methacrylon@gmail.com>
2019-05-18 18:17:37 +0200

    Change default width and height so it renderers well in cli by default

--- forest_fire.py
+++ forest_fire.py
@@ -44,8 +44,8 @@ def main():
     parser.add_argument('--number_steps', type=positive_int, help="Number of steps before the simulation ends", default=50)
     parser.add_argument('--planting_rate', type=probability, help="Probability of a tree to grow on an empty cell", default=0.1)
     parser.add_argument('--lightning_rate', type=probability, help="Probability of a light to strike each step", default=0.4)
-    parser.add_argument('-W', '--width', type=positive_int, help="Width of the forest", default=96)
-    parser.add_argument('-H', '--height', type=positive_int, help="Height of the forest", default=72)
+    parser.add_argument('-W', '--width', type=positive_int, help="Width of the forest", default=24)
+    parser.add_argument('-H', '--height', type=positive_int, help="Height of the forest", default=18)
     parser.add_argument('--renderer', help="Which renderer to use", default='cli')
     args = parser.parse_args()
     update_rate = args.update_rate
