ffb188445d7de134735f1f7dfe4c225af0f8686d
Methacrylon <methacrylon@gmail.com>
2019-05-18 17:21:59 +0200

    Add test for the different ash states

--- tests/test_debug_renderer.py
+++ tests/test_debug_renderer.py
@@ -43,6 +43,84 @@ class TestDebugRender:
         output = captured.out[:~0] # Remove trailing newline
         assert output == ONE_BURNING_GRID
 
+    def test_all_ash_phase(self, capsys):
+        START = """-1-1-1-1-1
+-1-1-1-1-1
+-1-15-1-1
+-1-1-1-1-1
+-1-1-1-1-1
+"""
+        STEP_1 = """-1-1-1-1-1
+-1555-1
+-1545-1
+-1555-1
+-1-1-1-1-1
+"""
+        STEP_2 = """55555
+54445
+54345
+54445
+55555
+"""
+        STEP_3 = """44444
+43334
+43234
+43334
+44444
+"""
+        STEP_4 = """33333
+32223
+32123
+32223
+33333
+"""
+        STEP_5 = """22222
+21112
+21012
+21112
+22222
+"""
+        STEP_6 = """11111
+10001
+10001
+10001
+11111
+"""
+        grid = gd.Grid(5, 5, planting_rate=0, lightning_rate=0)
+        for x in range(grid.width):
+            for y in range(grid.height):
+                grid[x, y] = gd.TREE
+        grid[2, 2] = gd.BURNING
+        renderer = debug_rd.DebugRenderer(grid, update_rate=0)
+        renderer.display()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == START
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_1
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_2
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_3
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_4
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_5
+        renderer.update()
+        captured = capsys.readouterr()
+        output = captured.out[:~0] # Remove trailing newline
+        assert output == STEP_6
+
 
     def test_time_update(self, mocker):
         mocker.patch('src.renderer.base.sleep')
@@ -79,7 +157,7 @@ class TestDebugRender:
         mocker.patch('src.renderer.base.sleep')
         grid = gd.Grid(3, 5, 0.5, 0.7)
         random.seed(0)
-        renderer = debug_rd.DebugRenderer(grid, update_rate=1, number_steps=7)
+        renderer = debug_rd.DebugRenderer(grid, number_steps=7)
         renderer.render()
         captured = capsys.readouterr()
         output = captured.out[:~0] # Remove trailing newline
